0a16b61340bc64842fd40a322194cae88176ec73
nawar <nawaraltibi46@gmail.com>
2024-11-29 12:27:04 +0300

    Added orange button widget

--- lib/App/Color/Color.dart
+++ lib/App/Color/Color.dart
@@ -3,5 +3,6 @@ import 'package:order_application/App/Utils/GetHexColor.dart';
 
 class AppColors {
   // static Color primary = HexColor("05101A");
+  String orangeColor = '#F19434';
 
 }
---
+++ lib/Presentation/Widgets/OrangeButtonWidget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+class OrangeButtonWidget extends StatelessWidget {
+  // Add a function parameter
+  final VoidCallback function;
+
+  // Constructor to accept the function parameter
+  const OrangeButtonWidget({
+    super.key,
+    required this.function, // Make onPressed required
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 56.w,
+      height: 56.w,
+      child: ElevatedButton(
+        onPressed: function,
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Color(0xFFF19434),
+            elevation: 2,
+            padding: EdgeInsets.all(0)
+        ),
+        child: SvgPicture.asset(
+          'assets/images/arrow-right.svg',
+          width: 24.89.w,
+          height: 24.89.h,
+        ),
+      ),
+    );
+  }
+}
